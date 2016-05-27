7e536b54dac552686c9db51cf0b1cdfc98407b89
xshenpan <xshenpan@xshenpans-MacBook-Pro.local>
2016-05-27 21:18:58 +0800

    XBDownloader demo v0.1

---
+++ XBDownloader/07-断点下载/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  07-断点下载
+//
+//  Created by xshenpan on 16/5/20.
+//  Copyright © 2016年 xshenpan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
