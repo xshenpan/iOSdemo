1730e9f2302d3f3694636b9ffc4e935b48107244
xshenpan <xshenpan@xshenpans-MacBook-Pro.local>
2016-08-06 16:17:13 +0800

    Initial Commit

---
+++ XBAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBAlertView
+//
+//  Created by xshenpan on 16/8/6.
+//  Copyright © 2016年 xshenpan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XBAlertView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XBAlertView
+//
+//  Created by xshenpan on 16/8/6.
+//  Copyright © 2016年 xshenpan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
